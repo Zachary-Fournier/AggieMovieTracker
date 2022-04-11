c9cd6495a3852c8fe2ec4871c042c1ae649e7955
matrev <markanthonytrevinojr@gmail.com>
2022-04-11 16:15:17 -0500

    added pages & styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Navbar, NavbarBrand, NavItem, NavLink, Nav } from 'reactstrap';
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter basename="/">
+        <Navbar expand="md">
+          <NavbarBrand href="/" className="mr-auto">
+            Aggie Movie Tracker
+          </NavbarBrand>
+          <Nav className="me-auto"
+          navbar>
+            <NavItem>
+              <NavLink href="/#/profile">Profile</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+        <Routes>
+          <Route exact path="/" element={<HomePage />}/>
+          <Route path="/profile" element={<ProfilePage />}/>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
---
+++ frontend/src/Utilities.js
@@ -0,0 +1,5 @@
+let url = "https://localhost:5000"
+export async function getMovies() {
+    let movies = fetch(`${url}/get-movies`)
+        .then()
+}
