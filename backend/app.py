915796cbed827c13338838bb93380c657a40cf92
Mahir Khan <mahirkhan0828@Mahirs-MacBook-Air-2.local>
2022-04-11 16:30:26 -0500

    Added postgres data

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World"
-
+# CONNECTING TO POSTGRES, CAN USE THIS TO PUT DATA IN VARIABLES AND ADD TO ROUTES
+conn = psycopg2.connect("dbname=310-Project user=postgres password=postgres")
+cur = conn.cursor()
+cur.execute("SELECT * from movies LIMIT 10;")
+print(cur.fetchone())
 
 if __name__ == '__main__':
     app.run()
