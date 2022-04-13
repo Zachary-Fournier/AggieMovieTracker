f545c21fd0747820983e0ece7e232763187f7772
Mahir Khan <mahirkhan0828@Mahirs-MacBook-Air-2.local>
2022-04-13 09:25:03 -0500

    Fixed backend server

--- backend/app.py
+++ backend/app.py
@@ -8,10 +8,16 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World"
 # CONNECTING TO POSTGRES, CAN USE THIS TO PUT DATA IN VARIABLES AND ADD TO ROUTES
-conn = psycopg2.connect("dbname=310-Project user=postgres password=postgres")
+# conn = psycopg2.connect("dbname=310-Project user=postgres password=postgres")
+conn = psycopg2.connect(
+    host="34.68.45.235",
+    database="postgres",
+    user="postgres",
+    password="postgres")
 cur = conn.cursor()
-cur.execute("SELECT * from movies LIMIT 10;")
-print(cur.fetchone())
-
+cur.execute("SELECT COUNT(tconst) FROM moviesreal")
+lst = cur.fetchall()
+for i in range(len(lst)):
+    print(lst[i])
 if __name__ == '__main__':
     app.run()
