5f9e5de4daf00a32eb6236efa4a3e8c2acf4ebb8
Mahir Khan <mahirkhan0828@Mahirs-MacBook-Air-2.local>
2022-04-19 12:56:00 -0500

    Added a bit of functionality to profile page, created the get user route where we can display the info based on username, created the user database in pgadmin

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,14 +1,50 @@
 import React, { useEffect, useState } from 'react'
+import { Button, Input, Spinner } from 'reactstrap';
 import { getUserInfo } from '../Utilities';
 
 export default function ProfilePage() {
+  const [searchInput, setSearchInput] = useState("John Doe");
+  const [userName, setUserName] = useState("Name")
+  const [userAge, setUserAge] = useState("Age");
+  const [userFavMovie, setFavMovie] = useState("Favorite Movie");
+  const [userNumMovies, setNumMovies] = useState("Number of Movies Watched");
+  const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <div>
-      <p>Name</p>
-      <p>Age</p>
-      <p>Favorite Movie</p>
-      <p>Number of movies watched</p>
-    </div>
-  )
+  async function onSearchButtonClick() {
+    console.log("RAN");
+    setIsLoading(true);
+    let results = await getUserInfo(searchInput);
+    setIsLoading(false);
+
+
+    
+    setUserName(results.user_name);
+    setUserAge(results.age);
+    setFavMovie(results.favMovie);
+    setNumMovies(results.numMovies);
+
+
+    console.log(userName);
+  }
+
+    return (
+      <div>
+  
+        <Button onClick={onSearchButtonClick}>Dummy Button for user John Doe!</Button>
+
+        <br/>
+        {isLoading &&
+        <Spinner></Spinner>}
+        <br/>
+
+        <p>{userName}</p>
+        <p>{userAge}</p>
+        <p>{userFavMovie}</p>
+        <p>{userNumMovies}</p>
+        
+      </div>
+    )
+  
+
+  
 }
