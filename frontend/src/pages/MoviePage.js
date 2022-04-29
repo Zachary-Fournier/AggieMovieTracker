1bce8e958e9aec7f67351e465bfe676d2a1c5b6a
matrev <markanthonytrevinojr@gmail.com>
2022-04-28 21:47:27 -0500

    favoriting

--- frontend/src/pages/MoviePage.js
+++ frontend/src/pages/MoviePage.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState} from 'react'
-import { Button } from 'reactstrap';
+import { Alert, Button, Input } from 'reactstrap';
 import { getMovieInfo, getMoviePoster, getUserInfo, updateFavoriteMovie } from '../Utilities';
 
 export default function MoviePage() {
 
     const [movieInfo, setMovieInfo] = useState([]);
     const [moviePosterURL, setMoviePosterURL] = useState();
+    const [ratingStars, setRatingStars] = useState(0);
+    const [didFavoriteWork, setDidFavoriteWork] = useState(0);
     const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
     async function getMovie() {
@@ -20,13 +22,14 @@ export default function MoviePage() {
     }
 
     async function updateFavMovie() {
-    
         let result = await updateFavoriteMovie(userInfo.userID, movieInfo[0]);
         if(result.response === "Success") {
             userInfo.favMovie = movieInfo;
             localStorage.setItem('userInfo', JSON.stringify(userInfo));
+            setDidFavoriteWork(1);
+        } else {
+            setDidFavoriteWork(0);
         }
-        console.log(result);
     }
     
 
@@ -48,11 +51,23 @@ export default function MoviePage() {
                     <p>avg rating</p> <br/>
                     <img src={moviePosterURL} style={{width: "25%", height: "50%"}}>
                     </img>
+                    <Input
+                    type='number'
+                    value={ratingStars}
+                    onChange={(e) => {setRatingStars(e.target.value)}}
+                    >
+                    </Input>
                     <Button onClick={rateMovie}>Leave a Rating!</Button>
                     {userInfo.favMovie === null ?
                         <Button onClick={updateFavMovie}>Favorite movie!</Button>
                         :
                         <Button onClick={updateFavMovie}>Make this your new favorite movie!</Button>
+                    }
+                    {didFavoriteWork === 1 &&
+                    <Alert>'{movieInfo[2]}' is now your favorite movie!</Alert>
+                    }
+                    {didFavoriteWork === -1 &&
+                        <Alert color='danger'>An error occurred, please try again!</Alert>
                     }
                     {/* <Button onClick={addToWatchlist()}>Add to watchlist!</Button> */}
                 </div>
